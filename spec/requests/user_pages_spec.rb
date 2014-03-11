b680e610d6cb36025a2b5d5c30c64b988da07f0a
LOZORD <ljrudberg@gmail.com>
2014-03-10 23:01:52 -0500

    Added another test for non-empty password confirmation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -115,6 +115,18 @@ describe "User pages" do
 				end
 			end
 
+			describe "confirmation is empty" do
+				before do
+					fill_in "Name",			with:"Example"
+					fill_in "Email",		with:"user@example.com"
+					fill_in "Password", with:"foobar"
+					fill_in "Confirmation",	with:""
+				end
+	
+				it "should not create a user" do
+					expect { click_button submit }.not_to change(User, :count)
+				end
+			end
 
     end
 
