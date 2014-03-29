fa73af8ee1cba87c9066b79903d6765aab87cbee
LOZORD <ljrudberg@gmail.com>
2014-03-29 18:22:49 -0500

    Did ex 8.5.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #render 'new'
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:email].downcase)
 
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:password])
       # sign in user and redirect to their page
       sign_in user
       redirect_to user
