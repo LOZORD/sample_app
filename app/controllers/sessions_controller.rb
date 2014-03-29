ec028830135b83ff29579c56fe14dcb82e8ab9c9
LOZORD <ljrudberg@gmail.com>
2014-03-29 15:45:29 -0500

    Fixed invalid signin, onto 8.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # sign in user and redir
+    else
+      # create error message and redirect signin page
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+
   end
 
   def destroy
