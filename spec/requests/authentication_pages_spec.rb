303c3161259b358ef655090cff34d3fd936b8e3a
LOZORD <ljrudberg@gmail.com>
2014-04-12 10:32:47 -0500

    Starting 9.5

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -106,6 +106,18 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end #as wrong user
+  
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end # DELETE request
+    end # non-admin
   end #authorization
 end # file
 
