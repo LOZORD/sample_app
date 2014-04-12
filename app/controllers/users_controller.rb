af3cb6ba72e72af34ae810ff91568aa9a6a8187f
LOZORD <ljrudberg@gmail.com>
2014-04-12 12:05:33 -0500

    Did 9.6.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,12 @@ class UsersController < ApplicationController
   end
 
   def new
-		@user = User.new
+		if signed_in?
+      flash[:warning] = 'You already have an account!'
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
 	def index
@@ -17,14 +22,20 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(user_params)
-		if @user.save
-      sign_in @user
-			flash[:success] = "Welcome, #{@user.name}, to the app!"
-			redirect_to @user
-		else
-		  render 'new'
-		end
+
+    if signed_in?
+      flash[:warning] = 'You already have an account!'
+      redirect_to root_path
+    else
+		  @user = User.new(user_params)
+		  if @user.save
+        sign_in @user
+			  flash[:success] = "Welcome, #{@user.name}, to the app!"
+			  redirect_to @user
+		  else
+		    render 'new'
+		  end
+    end
 	end
 
   def edit
