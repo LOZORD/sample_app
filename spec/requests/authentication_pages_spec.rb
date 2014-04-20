b15bcb7c2cf88ac6d8fd90ed480768a358413e68
LOZORD <ljrudberg@gmail.com>
2014-04-20 18:32:12 -0500

    AG, s 11.2.5

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -138,6 +138,19 @@ describe "Authentication" do
         end
 
       end #users controller
+
+      describe 'in the Relationships controller' do
+        describe 'submitting to the create actions' do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'submitting to the destroy action' do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end # relationship
+
     end #non-signed in users
 
     describe "as wrong user" do
