f518333c6ec9fc5ee88e4a14e038d79909f50069
LOZORD <ljrudberg@gmail.com>
2014-04-20 17:19:23 -0500

    AG, s 11.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ ThirdChapter::Application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
