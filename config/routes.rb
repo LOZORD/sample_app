e44e727c4360a7613dc0a219a0b435fd72243f4b
LOZORD <ljrudberg@gmail.com>
2014-04-20 16:59:14 -0500

    AG, @ listing 11.19

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 ThirdChapter::Application.routes.draw do
   #get "users/new"
 
-	resources :users
+	resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
 
