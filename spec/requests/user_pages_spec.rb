88029d9b76d3d33a530a9292c3ac614c610c0531
LOZORD <ljrudberg@gmail.com>
2014-04-20 17:43:13 -0500

    AG, s 11.2.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -282,4 +282,31 @@ describe "User pages" do
 
   end # user attempts to delete other
 
+  describe 'following/followers' do
+    let(:user)        { FactoryGirl.create(:user) }
+    let(:other_user)  { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe 'followed users' do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+
+      it { should have_title(full_title('Following')) }
+      it { should have_selector('h3', text: 'Following') }
+      it { should have_link(other_user.name, href: user_path(other_user)) }
+    end # following
+
+    describe 'followers' do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it { should have_title(full_title('Followers')) }
+      it { should have_selector('h3', text: 'Followers') }
+      it { should have_link(user.name, href: user_path(user)) }
+    end # followers 
+  end # follow
 end #rspec test
